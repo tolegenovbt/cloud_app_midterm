93f376a8984f5ed8c2e43254bd3e92ea02cded19
tolegenovbt <47154094+tolegenovbt@users.noreply.github.com>
2021-10-24 18:46:42 +0600

    Update config.js

--- cloudMidterm/config.js
+++ cloudMidterm/config.js
@@ -1,14 +1,14 @@
 const config = {};
 
-config.storage_connection_string = "DefaultEndpointsProtocol=https;AccountName=storbekzat;AccountKey=FZTxGq0Vq5L5G/bJ8OXaxoG1H04F58Slzk/8zqGYNdU9y23g9Tl5Hzkki+BJRNuSuY9Md6mT2LhDJDktTQW9yA==;EndpointSuffix=core.windows.net"
-config.sas_token = "sp=r&st=2021-10-23T07:00:10Z&se=2021-10-30T15:00:10Z&sv=2020-08-04&sr=c&sig=2mOMJpkMS3V0KvF%2BwC7QZAe7ZygTszsvc2lBtMcBz2w%3D"
-config.storContainerName = "images"
-config.storThumbContainerName = "thumbnails"
-config.sas_token_thumb = "sp=r&st=2021-10-23T16:36:39Z&se=2021-10-30T00:36:39Z&spr=https&sv=2020-08-04&sr=c&sig=Av0vQDHuMzCsOJdaCdie9A3luP3G2q6Z3YqIAMeyC2Y%3D"
-config.host = "https://dbbekzat.documents.azure.com:443/";
-config.authKey = "kknngJiLmx9Y0zWMKKrt74qHoExhh9Ufkqo9cYOG3gccBAmIx04EA0qyWJhJQ4WXehm29aexYKTPatsWeWLQfw==";
-config.databaseId = "Yachts";
-config.containerId = "Items";
+config.storage_connection_string = ""
+config.sas_token = """
+config.storContainerName = ""
+config.storThumbContainerName = ""
+config.sas_token_thumb = ""
+config.host = ""
+config.authKey = ""
+config.databaseId = ""
+config.containerId = ""
 
 if (config.host.includes("https://localhost:")) {
   console.log("Local environment detected");
